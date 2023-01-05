45e18a5b5457238a43ed0002800eeab8053739b5
James Leakos <jamesleakos@gmail.com>
2023-01-05 13:01:26 -0700

    new loader.io key

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.get('/products/:product_id/related', (req, res) => {
   controller.getRelated(req, res);
 });
 
-app.get('/loaderio-ffdd808ad5a04fb77110069a364e0303', (req, res) => {
-  res.send('loaderio-ffdd808ad5a04fb77110069a364e0303');
+app.get('/loaderio-fbc48b63fbfae9cf927007edb12a4609', (req, res) => {
+  res.send('loaderio-fbc48b63fbfae9cf927007edb12a4609');
 });
 
 const PORT = process.env.PORT || 3000;
